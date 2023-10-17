80628b6cacdd83d6cd36c16ab08173bcbc2a5ff0
Swapnil <rupakheteeswapnil@gmail.com>
2023-10-17 18:02:50 +0545

    3rd commit

--- src/App.js
+++ src/App.js
@@ -42,12 +42,15 @@ export default function App() {
 		},
 	];
   const[currentQuestion,setCurrentQuestion] =useState(0);
+  const[showScore,setShowScore] = useState(false);
   const handleAnswerButtonClick = () =>{
     const nextQuestion = currentQuestion+1;
-    
+
     if(nextQuestion < questions.length  ){
       setCurrentQuestion(nextQuestion);
 
+    }else{
+      setShowScore(true);
     }
     
     
@@ -57,7 +60,7 @@ export default function App() {
 		<div className='app'>
 			{/* HINT: replace "false" with logic to display the 
       score when the user has answered all the questions */}
-			{false ? (
+			{showScore ? (
 				<div className='score-section'>You scored 1 out of {questions.length}</div>
 			) : (
 				<>
